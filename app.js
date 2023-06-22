2bbc9dbdf70213beb1e832427324666e2b2e7be5
deepak.kus <deepak09.singh@gmail.com>
2023-06-22 18:08:37 +0530

    Api code updted

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ var express = require('express');
 var mongoose = require("mongoose");
 var cors = require('cors');
 var app = express();
+var logger = require('morgan');
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(logger('dev'));
 
 app.listen(8090, () => {
   console.log("Server is running on port 8090.");
